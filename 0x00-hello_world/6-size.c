d011112d3e15d88842ccd2a0017e9283c365faed
ZewdieMc <zewdiehabtie26@gmail.co>
2022-06-28 02:32:01 -0700

    over size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,9 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long in"
-			"t: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte("
-				"s)\n", sizeof(char), sizeof(int),sizeof(long), sizeof(double), sizeof(float));
+	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\n"
+			"Size of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\n"
+			"Size of a float: %d byte(s)\n"
+			, sizeof(char), sizeof(int), sizeof(long), sizeof(double), sizeof(float));
 	return (0);
 }
