43710de18ed8b2c262d0a38b09d44b5d2178859a
ZewdieMc <zewdiehabtie26@gmail.co>
2022-06-28 02:17:45 -0700

    done size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long" 
-			" int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte("
+	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long in"
+			"t: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte("
 				"s)\n", sizeof(char), sizeof(int),sizeof(long), sizeof(double), sizeof(float));
 	return (0);
 }
