8096d1dd9ab4ebca3e2a8f1efba9352b1380190c
ZewdieMc <zewdiehabtie26@gmail.co>
2022-07-01 06:33:48 -0700

    printf alphabet code update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char c;
 
-	for(c='a';c<='z';c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 	putchar(c);
 	}
