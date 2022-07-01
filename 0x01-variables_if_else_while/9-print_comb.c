7ef9d4b9644cd929d1946351d34d0bc51f74d7c9
ZewdieMc <zewdiehabtie26@gmail.co>
2022-07-01 12:26:45 -0700

    print digits combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,14 @@ int main(void)
 	int d;
 
 	for (d = 48; d < 58; d++)
-		putchar(d);
-		if (d < 57)
-		{
+	{
+	putchar(d);
+	if (d < 57)
+	{
 		putchar(',');
 		putchar(' ');
-		}
+	}
+	}
 		putchar('\n');
 	return (0);
 }
