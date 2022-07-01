7698ac1eb616e025b04b9714ddcb2b1759dde6a0
ZewdieMc <zewdiehabtie26@gmail.co>
2022-07-01 12:49:00 -0700

    possible combination of two numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x < 58; x++)
+	{
+	for (y = 48; y < 58; y++)
+	{
+	if (x < y)
+	{
+		putchar(x);
+		putchar(y);
+	}
+	if (x != 56 && y != 57)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
