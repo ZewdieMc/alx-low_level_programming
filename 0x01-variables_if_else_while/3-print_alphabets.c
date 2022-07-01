fd75e2cf256e35aa84bb7a30e664c13c1d9f86ba
ZewdieMc <zewdiehabtie26@gmail.co>
2022-07-01 06:51:59 -0700

    print alphabet in lowercase and uppercase.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is a longer description
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s, l;
+
+	for (s = 'a'; s <= 'z'; s++)
+	{
+	putchar(c);
+	}
+	for (l = 'A'; l <= 'Z'; l++)
+	{
+	putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
