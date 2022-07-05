3f89b1c50f68d91ce540af07f736191f9b48980b
ZewdieMc <zewdiehabtie26@gmail.co>
2022-07-05 12:37:55 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	char s[] = "_putchar";
+	char *s = "_putchar";
 
 	int i;
 
 	for (i = 0; i < 8; i++)
 	{
-		_putchar(s[i]);	
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 	return (0);
