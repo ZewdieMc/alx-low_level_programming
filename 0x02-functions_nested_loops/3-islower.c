a74d87563499b81c2c0842efe8dec81b4282a47b
ZewdieMc <zewdiehabtie26@gmail.co>
2022-07-05 13:17:47 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * islower - checks if a character is lower or  not
+ *
+ * return: 0 or 1
+ */
+int _islower(int c)
+{
+ if (c <= 122 && c >= 97)
+	 return 1;
+ else 
+	 return 0;
+
+}
