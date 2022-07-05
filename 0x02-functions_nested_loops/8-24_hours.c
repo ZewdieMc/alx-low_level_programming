0251996c66bcf36a40405d5c5aabe3b2ed9e7e21
ZewdieMc <zewdiehabtie26@gmail.co>
2022-07-05 14:36:29 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * 24_hours: prints every minute of the 24 hours
+ * jack_baue: prints every minute of the 24 hours
  *
  * Return: void
  */
@@ -9,6 +9,7 @@ void jack_bauer(void)
 {
 
 	int i, j;
+	
 	for (j = 0; j < 24; j++)
 	{
 		for (i = 0; i < 60; i++)
