629d9c57546bce707737561fccd2e4e22484205e
ZewdieMc <zewdiehabtie26@gmail.co>
2022-07-07 07:49:46 -0700

    test positive or neegative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - checks if positive_or_negative() prints correct output when provided with 0
+ * Return: 0
+ */
+int main(void)
+{
+
+	positive_or_negative(0);
+	return (0);
+}
