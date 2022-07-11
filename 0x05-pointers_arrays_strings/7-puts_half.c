2a7d103d686a7b878b4112ba3c6cdc1ff08b3905
ZewdieMc <zewdiehabtie26@gmail.co>
2022-07-11 03:28:11 -0700

    print the second half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,10 +10,8 @@ void puts_half(char *s)
 
 	while (s[i] != '\0')
 		i++;
-	if (i % 2 != 0)
-		n = (i - 1) / 2;
-	else
-		n = i / 2;
+
+	n = (i - 1) / 2;
 	for (j = n + 1; j < i; j++)
 		_putchar(s[j]);
 	_putchar('\n');
